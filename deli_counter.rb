76c91ca95a9c955f1036ee648ef97156543e10c1
Bikram Sodhi <biksodhi@gmail.com>
2020-03-10 15:10:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 def line(katz_deli)
   if katz_deli.length == 0
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each_with_index(1) do |person, i|
+    katz_deli.each.with_index(1) do |person, i|
       current_line << "#{i}. #{person}"
     end
     puts "current_line"
