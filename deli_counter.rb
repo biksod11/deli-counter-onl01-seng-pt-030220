dec34e5176b27810ef8954ea3a8f8e2b3b4f6a27
Bikram Sodhi <biksodhi@gmail.com>
2020-03-10 15:22:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, new_person)
   katz_deli << new_person
-  puts "Welcome, #{"new_person"}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{new_person}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
